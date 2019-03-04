d511cb25ef41885401e30beec0a8752bc0eb1a9f
Victor Filimon <viktor_filim08@yahoo.com>
2019-03-04 22:33:11 +0200

    -

--- Lab1/createFiles.js
+++ Lab1/createFiles.js
@@ -4,7 +4,6 @@ function createFile(size, fileName){
     var indexSize = size;
     for(let i=0; i<= indexSize; i++) {
       file.write('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n');
-      file
     }
     file.end();
     file.on('finish', () => {
@@ -14,4 +13,4 @@ function createFile(size, fileName){
 // createFile(1e6, './files/small.file');
 // createFile(1e6*2, './files/medium.file');
 // createFile(1e6*4, './files/large.file');
- createFile(1e6, './files/hugee.file');
+ createFile(1e6*4, './files/hugee.file');
